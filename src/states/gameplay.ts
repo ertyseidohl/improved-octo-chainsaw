4efc85fe2ba150b88a28cf0db517027787e262ae
Erty Seidohl <ertysdl+github@gmail.com>
2018-12-01 12:54:39 -0500

    add floor tiles

--- src/states/gameplay.ts
+++ src/states/gameplay.ts
@@ -1,3 +1,9 @@
+import { PhaserTextStyle } from "phaser-ce";
+
+const NUM_TILE_SPRITES = 9;
+const ENGINEERING_TILES_WIDTH = 10;
+const ENGINEERING_TILES_HEIGHT = 10;
+
 export default class Startup extends Phaser.State {
     private cursors: Phaser.CursorKeys;
     private mario: Phaser.Sprite;
@@ -5,14 +11,34 @@ export default class Startup extends Phaser.State {
     private shmupBounds: Phaser.Rectangle;
     private engineeringBounds: Phaser.Rectangle;
 
+    private engineeringTiles: Phaser.Group;
+
     private borderSprite: Phaser.Sprite;
 
     public preload(): void {
         this.game.load.image("mario", "../assets/mario.png");
         this.game.load.image("border", "../assets/border.png");
+
+        for (let i: number = 1; i <= NUM_TILE_SPRITES; i++) {
+            this.game.load.image(`floor_tile_${i}`, `../assets/floor_tile_${i}.png`);
+        }
     }
 
     public create(): void {
+        this.engineeringTiles = this.game.add.group();
+
+        const engineeringFloorStartX = this.game.width / 2 + 50;
+        const engineeringFloorStartY = 100;
+
+        for (let i: number = 0; i < ENGINEERING_TILES_WIDTH; i++) {
+            for (let j: number = 0; j < ENGINEERING_TILES_HEIGHT; j++) {
+                this.engineeringTiles.create(
+                    engineeringFloorStartX + (32 * i),
+                    engineeringFloorStartY + (32 * j),
+                    this.getTileSprite(),
+                );
+            }
+        }
 
         this.shmupBounds = new Phaser.Rectangle(0, 0, this.game.width / 2, this.game.height);
         this.engineeringBounds = new Phaser.Rectangle(this.game.width / 2, 0, this.game.width / 2, this.game.height);
@@ -52,5 +78,10 @@ export default class Startup extends Phaser.State {
 
     private updateEngineering(): void {
         // todo
+    }
+
+    private getTileSprite(): string {
+        const r: number = Math.floor(Math.random() * NUM_TILE_SPRITES) + 1;
+        return `floor_tile_${r}`;
     }
 }
