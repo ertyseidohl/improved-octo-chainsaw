0a09a686d9d9261066541169a35e18111c831bad
Erty Seidohl <ertysdl+github@gmail.com>
2018-12-01 12:15:00 -0500

    Just use a friggin border sprite

--- src/states/gameplay.ts
+++ src/states/gameplay.ts
@@ -5,21 +5,17 @@ export default class Startup extends Phaser.State {
     private shmupBounds: Phaser.Rectangle;
     private engineeringBounds: Phaser.Rectangle;
 
-    private shmupSprite: Phaser.Sprite;
-    private engineeringSprite: Phaser.Sprite;
+    private borderSprite: Phaser.Sprite;
 
     private shmupCollisionGroup: Phaser.Physics.P2.CollisionGroup;
     private engineeringCollisionGroup: Phaser.Physics.P2.CollisionGroup;
 
     public preload(): void {
         this.game.load.image("mario", "../assets/mario.png");
-        this.game.load.image("shmup_bg", "../assets/shmup_bg.png");
-        this.game.load.image("engineering_bg", "../assets/engineering_bg.png");
+        this.game.load.image("border", "../assets/border.png");
     }
 
     public create(): void {
-        this.game.physics.p2.setImpactEvents(true);
-        this.game.physics.p2.restitution = 0.8;
 
         this.shmupBounds = new Phaser.Rectangle(0, 0, this.game.width / 2, this.game.height);
         this.engineeringBounds = new Phaser.Rectangle(this.game.width / 2, 0, this.game.width / 2, this.game.height);
@@ -27,16 +23,10 @@ export default class Startup extends Phaser.State {
         this.shmupCollisionGroup = this.game.physics.p2.createCollisionGroup();
         this.engineeringCollisionGroup = this.game.physics.p2.createCollisionGroup();
 
-        // this.shmupSprite = this.game.add.sprite(0, 0, "shmup_bg");
-        // this.game.physics.p2.enable(this.shmupSprite);
-        // this.shmupSprite.body.static = true;
-        // this.shmupSprite.anchor = new Phaser.Point(0, 0);
-        // this.shmupSprite.body.setCollisionGroup(this.engineeringCollisionGroup);
-
-        this.engineeringSprite = this.game.add.sprite(this.game.width * 0.75, this.game.height / 2, "engineering_bg");
-        this.game.physics.p2.enable(this.engineeringSprite, true);
-        const engineeringSpriteBody: Phaser.Physics.P2.Body = this.engineeringSprite.body;
-        engineeringSpriteBody.static = true;
+        this.borderSprite = this.game.add.sprite(this.game.width / 2, this.game.height / 2, "border");
+        this.game.physics.p2.enable(this.borderSprite, true);
+        const borderSpriteBody: Phaser.Physics.P2.Body = this.borderSprite.body;
+        borderSpriteBody.static = true;
 
         this.mario = this.game.add.sprite(200, 200, "mario");
         this.mario.scale = new Phaser.Point(0.2, 0.2);
--- src/states/startup.ts
+++ src/states/startup.ts
@@ -2,6 +2,9 @@ export default class Startup extends Phaser.State {
     public create(): void {
         this.game.physics.startSystem(Phaser.Physics.P2JS);
 
+        this.game.physics.p2.setImpactEvents(true);
+        this.game.physics.p2.restitution = 0.8;
+
         // Disable Multitouch
         this.input.maxPointers = 1;
 
