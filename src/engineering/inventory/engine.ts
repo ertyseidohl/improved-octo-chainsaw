6d433934b87682d62b6d6ec989c8db3ca2920acf
Ryan McVerry <ryan.mcverry@gmail.com>
2018-12-01 22:54:10 -0500

    fixed animation

--- src/engineering/inventory/engine.ts
+++ src/engineering/inventory/engine.ts
@@ -4,7 +4,7 @@ import { InventorySystem } from "./system";
 export class Engine extends BaseComponent {
 
     constructor(game: Phaser.Game, inventorySystem: InventorySystem, x: number, y: number) {
-        super(game, inventorySystem, x, y, "engine_1_live", 1, 2);
+        super(game, inventorySystem, x, y, "engine_1", 1, 2);
 
         const engineAnimation: Phaser.Animation = this.animations.add("burn", [1, 2, 3, 4]);
         engineAnimation.play(20, true);
