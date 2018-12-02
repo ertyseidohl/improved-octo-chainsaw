aa26b05da84a6d9a9c2fd306c659cc2de78a4391
Ryan McVerry <ryan.mcverry@gmail.com>
2018-12-02 14:41:12 -0500

    rough

---
+++ src/engineering/inventory/power_subsystem.ts
@@ -0,0 +1,28 @@
+import { BaseComponent } from "./base_component";
+
+class PowerSubSystem {
+
+    private sourceToSinkMap: Map<BaseComponent, Set<BaseComponent>>;
+
+    private sinkToSourceMap: Map<BaseComponent, Set<BaseComponent>>;
+
+    public attach(source: BaseComponent, sink: BaseComponent) {
+        const sinkSet = this.sourceToSinkMap.get(source);
+        sinkSet.add(sink);
+
+        const sourceSet = this.sinkToSourceMap.get(sink);
+        sourceSet.add(source);
+    }
+
+    public detach(source: BaseComponent, sink: BaseComponent) {
+
+        const sinkSet = this.sourceToSinkMap.get(source);
+        sinkSet.delete(sink);
+
+        const sourceSet = this.sinkToSourceMap.get(sink);
+        sourceSet.add(source);
+    }
+
+    
+
+}
