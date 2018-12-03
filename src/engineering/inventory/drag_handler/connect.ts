f55508aea95cd5901d9d0856334e075fabda6409
Tim Douglas <tdouglas4@bloomberg.net>
2018-12-02 19:49:17 -0500

    Add connection pads, ability to re-drag

--- src/engineering/inventory/drag_handler/connect.ts
+++ src/engineering/inventory/drag_handler/connect.ts
@@ -1,12 +1,16 @@
 // DEPENDENCIES
 import { BaseComponent } from "../base_component";
 import { BaseDragHandler } from "./base";
-
 import { InventorySystem } from "../system";
 import { PowerSubSystem } from "../power_subsystem";
 
 // TYPES
-class RopeWire extends Phaser.Rope {
+class RopeWire extends Phaser.Group {
+
+    public sourcePad: Phaser.Sprite;
+    public sinkPad: Phaser.Sprite;
+
+    private rope: Phaser.Rope;
 
     constructor(
         game: Phaser.Game,
@@ -15,8 +19,10 @@ class RopeWire extends Phaser.Rope {
         private source: Phaser.Point,
         sink: Phaser.Point,
     ) {
-        super(
-            game,
+        super(game);
+        parent.addChild(this);
+
+        this.rope = this.add(this.game.add.rope(
             source.x,
             source.y,
             "wire",
@@ -25,15 +31,24 @@ class RopeWire extends Phaser.Rope {
                 new Phaser.Point(0, 0),
                 new Phaser.Point(0, 0),
             ],
-        );
+        ));
+        const padBitmap = this.game.add.bitmapData(10, 10);
+        padBitmap.fill(0, 0, 255);
+        this.sourcePad = this.add(this.game.add.sprite(source.x - 5,
+                                                       source.y - 5,
+                                                       padBitmap));
+        this.sinkPad = this.add(this.game.add.sprite(source.x - 5,
+                                                     source.y - 5,
+                                                     padBitmap));
         parent.addChild(this);
     }
 
     set sinkPoint(value: Phaser.Point) {
-        this.points = [
+        this.rope.points = [
             new Phaser.Point(0, 0),
-            value.clone().subtract(this.x, this.y),
+            value.clone().subtract(this.rope.x, this.rope.y),
         ];
+        this.sinkPad.position = value.clone().subtract(5, 5);
     }
 }
 
@@ -44,12 +59,18 @@ interface ConnectionState {
     wire: Wire;
 }
 
+interface ConnectionLink {
+    source: BaseComponent;
+    sink: BaseComponent;
+}
+
 interface Coordinate {
     x: number;
     y: number;
 }
 
 interface PendingConnect {
+    source: BaseComponent;
     start: Coordinate;
     wire: Wire;
 }
@@ -65,7 +86,7 @@ class Connections {
     // PUBLIC METHODS
     public tryConnect(
         source: Phaser.Sprite,
-        target: Phaser.Sprite,
+        sink: Phaser.Sprite,
         wire: Wire,
     ): boolean {
         let existing = this.entries.get(source);
@@ -73,11 +94,18 @@ class Connections {
             existing = new Map();
             this.entries.set(source, existing);
         }
-        if (existing.has(target)) {
+        if (existing.has(sink)) {
             return false;
         }
-        existing.set(target, { wire });
+        existing.set(sink, { wire });
         return true;
+    }
+
+    public disconnect(source: Phaser.Sprite, sink: Phaser.Sprite) {
+        const existing = this.entries.get(source);
+        if (existing) {
+            existing.delete(sink);
+        }
     }
 
 }
@@ -88,6 +116,7 @@ export class ConnectDragHandler extends BaseDragHandler {
     private connections: Connections = new Connections();
     private pendingConnect: PendingConnect | null = null;
     private wiresGroup: Phaser.Group;
+    private connectedWires = new Map<Wire, ConnectionLink>();
 
     // CREATORS
     constructor(
@@ -104,6 +133,7 @@ export class ConnectDragHandler extends BaseDragHandler {
         // find sprite
         const p = this.game.input.mousePointer;
         this.pendingConnect = {
+            source: comp,
             start: { x: comp.x, y: comp.y },
             wire: new Wire(
                 this.game,
@@ -121,7 +151,7 @@ export class ConnectDragHandler extends BaseDragHandler {
         }
         const { wire } = this.pendingConnect;
         const p = this.game.input.mousePointer;
-        const sink = this.inventorySystem.find(p.position);
+        const sink = this.inventorySystem.find(p);
         if (undefined === sink) {
             this.wires.remove(wire, true);
         } else if (comp === sink) {
@@ -129,6 +159,12 @@ export class ConnectDragHandler extends BaseDragHandler {
         } else if (!this.connections.tryConnect(comp, sink, wire)) {
             this.wires.remove(wire, true);
         } else {
+            wire.sinkPad.inputEnabled = true;
+            wire.sinkPad.input.enableDrag();
+            wire.sinkPad.events.onDragStart.add(this.wireDragStart, this, null, wire);
+            wire.sinkPad.events.onDragStop.add(this.wireDragStop, this, null, wire);
+            wire.sinkPad.events.onDragUpdate.add(this.wireDragUpdate, this, null, wire);
+            this.connectedWires.set(wire, { source: comp, sink });
             this.powerSystem.attach(comp, sink);
             this.powerSystem.updateAllComponents();
         }
@@ -148,6 +184,48 @@ export class ConnectDragHandler extends BaseDragHandler {
     // PUBLIC PROPERTIES
     get wires(): Phaser.Group {
         return this.wiresGroup;
+    }
+
+    // PRIVATE METHODS
+    private wireDragStart(
+        obj: any,
+        pointer: Phaser.Pointer,
+        x: number,
+        y: number,
+        wire: Wire
+    ) {
+        console.log("drag start", arguments);
+        const p = this.game.input.mousePointer;
+        const { source, sink } = this.connectedWires.get(wire);
+        this.connectedWires.delete(wire);
+        this.connections.disconnect(source, sink);
+        this.powerSystem.detach(source, sink);
+        this.pendingConnect = {
+            source,
+            start: { x: wire.x, y: wire.y },
+            wire,
+        };
+    }
+
+    private wireDragStop(obj: any, p: Phaser.Pointer, wire: Wire) {
+        console.log("drag sop", arguments);
+        if (this.pendingConnect) {
+            this.dragStop(this.pendingConnect.source);
+        }
+    }
+
+    private wireDragUpdate(
+        obj: any,
+        p: Phaser.Pointer,
+        newX: number,
+        newY: number,
+        snappedTo: any,
+        fromStart: boolean,
+        dX: number,
+        dY: number,
+        wire: Wire,
+    ) {
+        wire.sinkPoint = p.position;
     }
 
 }
